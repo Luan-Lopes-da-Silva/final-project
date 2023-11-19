4d753335ac01ed33cf559c536a45f3b01b436ce6
Luan-Lopes-da-Silva <lopesluan18@outlook.com>
2023-11-19 11:55:29 -0300

    Fix error

--- app/api/page.tsx
+++
@@ -1,8 +0,0 @@
-import { NextApiRequest, NextApiResponse } from 'next'
-import operacoes from '../../db.json'
-
-export default function handler(req:NextApiRequest,res:NextApiResponse){
-return(
-  <h1>{operacoes.operations[0].financiamento}</h1>
-)
-}
